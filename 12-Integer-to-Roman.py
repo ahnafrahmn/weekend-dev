aa6aa720f1c3415f13106e2eb587c25b044c581d
ahnafrahmn <ahnafx24@gmail.com>
2025-12-13 13:01:49 +1100

    13/10/25

---
+++ 12-Integer-to-Roman.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def intToRoman(self, n):
+        l = [
+            (1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'), (100, 'C'), (90, 'XC'), 
+            (50, 'L'), (40, 'XL'), (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')
+        ]
+        r = ""
+        for val, sym in l:
+            while n >= val:
+                n -= val
+                r += sym
+        return r
